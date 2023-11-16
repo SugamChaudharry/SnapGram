979bff25217135a20237b13bc11478da3adb1dc0
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-16 06:50:01 +0530

    add signup form

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,15 +8,15 @@ function AuthLayout() {
       {isAunthenticated ? (
         <Navigate to="/" />
       ) : (
-      <>
-          <section className="flex justify-center items-center flex-col">
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
-          <img src="" alt="" />
+          <img src="/assets/images/side-img.svg" alt="logo" className=" hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
         </>
       )}
     </>
-  )
+  );
 }
 
 export default AuthLayout
