b7ef306f6bf85a4b101b394ac0193d2469c02762
SugamChaudharry <nextgenachieverss@gmail.com>
2023-11-18 13:50:18 +0530

    .

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -3,6 +3,10 @@ import { Client , Account, Databases, Storage, Avatars } from "appwrite";
 export const appwriteConfig = {
   url: import.meta.env.VITE_APPWRITE_URL,
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  saveCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 };
 
 export const client = new Client();
